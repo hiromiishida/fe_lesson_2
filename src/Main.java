f163b00e23efdd15885b5eb2064c6190546a008a
digitalplan <digitalplan21@gmail.com>
2022-10-08 00:05:26 +0900

    データ型と変数

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1    = 12;
+        double num2 = 12.345;
+        String str1 = "こんにちは";
+        char chr1   = 'A';
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(str1);
+        System.out.println(chr1);
+    }
+}
